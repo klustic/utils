0b82d1861966f642f39c9b2449145c7a19b29f43
Kevin Lustic <klustic@users.noreply.github.com>
2018-08-14 14:34:16 -0600

    Create curlscan.sh

---
+++ curlscan.sh
@@ -0,0 +1,4 @@
+# Paste this into your bash terminal, then use `curlscan` as a command:
+# $ curlscan
+# Usage: curlscan <ip|domain>
+curlscan() { if [ $# -ne 1 ]; then echo "Usage: curlscan <ip|domain>"; return; fi; for port in {1..65535}; do curl -m1 -s http://${1}:${port} &>/dev/null; case $? in 7) status=CLOSED;; 28) continue;; *) status=OPEN;; esac; printf "${1} %5d\t%s\n" ${port} ${status}; done; }
